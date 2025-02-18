f7d9239e507fcfbffe6b2e07bf4f8f84d5d6aed1
ramya72002 <nsriramya7@gmail.com>
2025-02-18 12:28:31 +0530

    dashboard changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: "http://localhost:3000",
+		baseURL: "https://chess-champs-fen-six.vercel.app",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
@@ -71,7 +71,7 @@ export default defineConfig({
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: "pnpm rsbuild preview",
-		url: "http://localhost:3000",
+		url: "https://chess-champs-fen-six.vercel.app",
 		reuseExistingServer: !process.env.CI,
 	},
 });
